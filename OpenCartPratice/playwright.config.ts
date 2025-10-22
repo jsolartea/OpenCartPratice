a2c52565418eb47a6674c2f482b863b575758ab0
jsolartea <jhonny.solarte@gmail.com>
2025-10-21 22:16:04 -0400

    change2

--- OpenCartPratice/playwright.config.ts
+++ OpenCartPratice/playwright.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: true,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
